a2aed0ffbab0b2d311c427e9634ad826ee7a039a
moegithub <moanisur@gmail.com>
2021-07-16 08:43:58 -0700

    Clickable Human body

---
+++ index.js
@@ -0,0 +1,80 @@
+let documetArea = document.querySelector('#area')
+console.log(documetArea)
+
+let humanHead = document.querySelector('.head')
+humanHead.addEventListener("click", (e)=>{
+    console.log("Head has been clicked") 
+    documetArea.innerText = "Area: Head had been clicked"
+})
+
+let leftShoulder = document.querySelector('.left-shoulder')
+leftShoulder.addEventListener("click", (e)=>{
+    console.log("Left Shoulder has been clicked")
+    documetArea.innerText = "Area: Left Shoulder has been clicked"
+})
+
+let rightShoulder = document.querySelector('.right-shoulder')
+rightShoulder.addEventListener("click", (e)=>{
+    console.log("Right Shoulder has been clickek")   
+    documetArea.innerText = "Area: Right Shoulder has been clicked"
+})
+
+let chest = document.querySelector('.chest')
+chest.addEventListener("click", (e)=>{
+    console.log("Chest has been clicked")
+    documetArea.innerText = "Area: Chest has been clicked"
+})
+
+let stomach = document.querySelector('.stomach')
+stomach.addEventListener("click", (e)=>{
+    console.log("Stomach has been clicked")
+    documetArea.innerText = "Area: Stomach has been clicked"
+})
+
+let leftArm = document.querySelector('.left-arm')
+leftArm.addEventListener("click", (e)=>{
+    console.log("Left arm has been clicked")
+    documetArea.innerText = "Area: Left Arm has been clicked"
+})
+
+let rightArm = document.querySelector('.right-arm')
+rightArm.addEventListener("click", (e)=>{
+    console.log("Right arm has been clicked")
+    documetArea.innerText = "Area: Right Arm been clicked"
+})
+
+let leftHand = document.querySelector('.left-hand')
+leftHand.addEventListener("click", (e)=>{
+    console.log("Left Hand has been clicked")
+    documetArea.innerText = "Area: Left Hand has been clicked"
+})
+
+let rightHand = document.querySelector('.right-hand')
+rightHand.addEventListener("click", (e)=>{
+    console.log("Right Hand has been clicked")
+    documetArea.innerText = "Area: Right Hand has been clicked"
+})
+
+let leftLeg = document.querySelector('.left-leg')
+leftLeg.addEventListener("click", (e)=>{
+    console.log("Left Leg has been clicked")
+    documetArea.innerText = "Area: Left Leg has been clicked"
+})
+
+let rightLeg = document.querySelector('.right-leg')
+rightLeg.addEventListener("click", (e)=>{
+    console.log("Right Leg has been clicked")
+    documetArea.innerText = "Area: Right Leg has been clicked"
+})
+
+let rightFoot = document.querySelector('.right-foot')
+rightFoot.addEventListener("click", (e)=>{
+    console.log("Right Foot has been clicked")
+    documetArea.innerText = "Area: Right Foot has been clicked"
+})
+
+let leftFoot = document.querySelector('.left-foot')
+leftFoot.addEventListener("click", (e)=>{
+    console.log("Left Foot has been clicked")
+    documetArea.innerText = "Area: Left Foot has been clicked"
+})
